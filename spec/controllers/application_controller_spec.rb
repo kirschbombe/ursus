f1760174bdffbd60b0293eda03b90d078bbf3e6e
darrowcoucla <darrowco@library.ucla.edu>
2020-02-06 15:42:39 -0800

    added more to the test

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -4,6 +4,21 @@ require 'rails_helper'
 RSpec.describe ApplicationController, type: :controller do
   let(:controller) { described_class.new }
 
+  before do
+    allow(Flipflop).to receive(:sinai?).and_return(true)
+    allow(controller).to receive(:redirect_target).and_return('/redirect_target')
+    allow(controller).to receive(:cookies).and_return(requested_path: '/requested_path')
+    allow(controller).to receive(:login_path).and_return('/test_login')
+    allow(controller).to receive(:redirect_to)
+    allow(controller).to receive(:request).and_return(instance_double('ActionDispatch::Request', path: '/'))
+    allow(controller).to receive(:set_auth_cookies)
+    allow(controller).to receive(:sinai_authenticated?).and_return(false)
+    allow(controller).to receive(:ucla_token?).and_return(true)
+    allow(controller).to receive(:version_path).and_return('/test_version')
+    allow(ENV).to receive(:[]).and_call_original
+    allow(ENV).to receive(:[]).with('SINAI_ID_BYPASS').and_return(nil)
+  end
+
   describe '#display_banner?' do
     context 'when banner cookie is set' do
       before do
@@ -36,18 +51,6 @@ RSpec.describe ApplicationController, type: :controller do
 
   describe '#sinai_authn_check' do
     context 'if ucla token is set in URL parameters' do
-      before do
-        allow(Flipflop).to receive(:sinai?).and_return(true)
-        allow(controller).to receive(:cookies).and_return(requested_path: '/requested_path')
-        allow(controller).to receive(:login_path).and_return('/test_login')
-        allow(controller).to receive(:redirect_to)
-        allow(controller).to receive(:request).and_return(instance_double('ActionDispatch::Request', path: '/'))
-        allow(controller).to receive(:set_auth_cookies)
-        allow(controller).to receive(:sinai_authenticated?).and_return(false)
-        allow(controller).to receive(:ucla_token?).and_return(true)
-        allow(controller).to receive(:version_path).and_return('/test_version')
-      end
-
       it 'calls set_auth_cookies' do
         controller.sinai_authn_check
         expect(controller).to have_received(:set_auth_cookies)
@@ -60,7 +63,13 @@ RSpec.describe ApplicationController, type: :controller do
     end
 
     context 'if ucla token is not set' do
-
+      before do
+        allow(controller).to receive(:ucla_token?).and_return(false)
+      end
+      it 'redirects to requested path' do
+        controller.sinai_authn_check
+        expect(controller).to have_received(:redirect_to).with('/redirect_target')
+      end
     end
 
     context 'if ENV[\'SINAI_ID_BYPASS\' is true' do
