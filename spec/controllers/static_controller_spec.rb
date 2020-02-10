b427e33cf6262deccf2a6a2bcf94b803f5d08fe0
Andrew Wallace <andrewwallace@library.ucla.edu>
2020-02-10 15:42:45 -0800

    controller tests for StaticController

--- spec/controllers/static_controller_spec.rb
+++ spec/controllers/static_controller_spec.rb
@@ -8,4 +8,46 @@ RSpec.describe StaticController, type: :controller do
       expect(response).to have_http_status(:success)
     end
   end
+
+  describe "GET #copyright" do
+    it "returns http success" do
+      get :copyright
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET #privacy" do
+    it "returns http success" do
+      get :privacy
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET #contact" do
+    it "returns http success" do
+      get :contact
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET #migration_updates" do
+    it "returns http success" do
+      get :migration_updates
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET #version" do
+    it "returns http success" do
+      get :version
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET #static_pages" do
+    it "returns http success" do
+      get :static_pages
+      expect(response).to have_http_status(:success)
+    end
+  end
 end
